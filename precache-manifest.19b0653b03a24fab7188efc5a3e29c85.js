9427eab6c407451a930e4ea157b8e87643cc004d
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-10-10 12:55:30 +0200

    Updates

---
+++ precache-manifest.19b0653b03a24fab7188efc5a3e29c85.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e2dbbc360e92bcd8ae67de0da0eb8c9",
+    "url": "/crypto-master/index.html"
+  },
+  {
+    "revision": "5a686ef14612ba090c6c",
+    "url": "/crypto-master/static/js/0.3270354c.chunk.js"
+  },
+  {
+    "revision": "a90976ec505f6dfa1d31",
+    "url": "/crypto-master/static/js/3.dcf01bb1.chunk.js"
+  },
+  {
+    "revision": "e9a0f5757f4e6c6a656a",
+    "url": "/crypto-master/static/js/4.5c153c66.chunk.js"
+  },
+  {
+    "revision": "8d52cae169cbb3ad4c29",
+    "url": "/crypto-master/static/js/5.1940be33.chunk.js"
+  },
+  {
+    "revision": "1e63a860d3a5d92bf082",
+    "url": "/crypto-master/static/js/6.4ff209c5.chunk.js"
+  },
+  {
+    "revision": "60448256a7eb3ca5d270",
+    "url": "/crypto-master/static/js/7.6c4e2d1c.chunk.js"
+  },
+  {
+    "revision": "982eb6ba1962f2fb392e",
+    "url": "/crypto-master/static/js/8.9a8776e4.chunk.js"
+  },
+  {
+    "revision": "bf3cfd9f44666dbe1819",
+    "url": "/crypto-master/static/js/9.c3045dbf.chunk.js"
+  },
+  {
+    "revision": "f7b9e64f62e0684ecd5a",
+    "url": "/crypto-master/static/js/main.168d15c6.chunk.js"
+  },
+  {
+    "revision": "9e40c6481090bb62ec34",
+    "url": "/crypto-master/static/js/runtime~main.584c0e2a.js"
+  },
+  {
+    "revision": "a429378d9e8284334b85f3d77382503a",
+    "url": "/crypto-master/static/media/ada.a429378d.svg"
+  },
+  {
+    "revision": "274d44f020650953069e6daaf4929b8b",
+    "url": "/crypto-master/static/media/bch.274d44f0.svg"
+  },
+  {
+    "revision": "f78cb0ac9030a7bec8aec4de2645b1fc",
+    "url": "/crypto-master/static/media/bnb.f78cb0ac.svg"
+  },
+  {
+    "revision": "709dc32e06d4435ff6f770d20c579bfb",
+    "url": "/crypto-master/static/media/bsv.709dc32e.svg"
+  },
+  {
+    "revision": "e73cb1b222d746763a3d57ab92c94621",
+    "url": "/crypto-master/static/media/btc.e73cb1b2.svg"
+  },
+  {
+    "revision": "951d489f21a869f815a6d354161bf41b",
+    "url": "/crypto-master/static/media/dash.951d489f.svg"
+  },
+  {
+    "revision": "d431754f66fcced10457921c2ecd8627",
+    "url": "/crypto-master/static/media/eos.d431754f.svg"
+  },
+  {
+    "revision": "fc290deb2b3b4cadbc8cfeef41a2fd46",
+    "url": "/crypto-master/static/media/etc.fc290deb.svg"
+  },
+  {
+    "revision": "18708d3e3f54116e2ea639fdd56dd6c5",
+    "url": "/crypto-master/static/media/eth.18708d3e.svg"
+  },
+  {
+    "revision": "c6c0c9ce87ec46006f38b7a7f68fea0d",
+    "url": "/crypto-master/static/media/facebook.c6c0c9ce.png"
+  },
+  {
+    "revision": "afb13f7b2e27ecee9ecfb9bcc851e24d",
+    "url": "/crypto-master/static/media/google.afb13f7b.png"
+  },
+  {
+    "revision": "cdae08509e949c7b6216a744b14f0087",
+    "url": "/crypto-master/static/media/ltc.cdae0850.svg"
+  },
+  {
+    "revision": "7237b8dcb407480ee15e98e8561d479f",
+    "url": "/crypto-master/static/media/miota.7237b8dc.svg"
+  },
+  {
+    "revision": "242f29f38b3dd8d5ce69061ef2acc4a4",
+    "url": "/crypto-master/static/media/mkr.242f29f3.svg"
+  },
+  {
+    "revision": "bef270c0e49aebf7ea92968ce524fb6a",
+    "url": "/crypto-master/static/media/neo.bef270c0.svg"
+  },
+  {
+    "revision": "d3eaadb30c18e1dab3c3e011449882d3",
+    "url": "/crypto-master/static/media/ont.d3eaadb3.svg"
+  },
+  {
+    "revision": "d986d6a266e2e1a3985c8ec98af30fa0",
+    "url": "/crypto-master/static/media/trx.d986d6a2.svg"
+  },
+  {
+    "revision": "67442b381f6c175839d509390c13e2b0",
+    "url": "/crypto-master/static/media/usd.67442b38.svg"
+  },
+  {
+    "revision": "f6f2a6e4599ecace9227a44283e1374d",
+    "url": "/crypto-master/static/media/xem.f6f2a6e4.svg"
+  },
+  {
+    "revision": "29ac2fa4e98cbd3cafe6d55bbee72938",
+    "url": "/crypto-master/static/media/xlm.29ac2fa4.svg"
+  },
+  {
+    "revision": "9749f02708e42f52a0ad631801edba5a",
+    "url": "/crypto-master/static/media/xmr.9749f027.svg"
+  },
+  {
+    "revision": "84ce7542e288e2bf98a4da4dbde2594e",
+    "url": "/crypto-master/static/media/xrp.84ce7542.svg"
+  },
+  {
+    "revision": "8c981a7963d84fa39b55e0da6a1cc898",
+    "url": "/crypto-master/static/media/xtz.8c981a79.svg"
+  }
+]);
