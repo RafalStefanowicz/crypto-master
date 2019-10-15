802446536dc9b27617636d53bf900dea6f0b6287
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-10-15 15:42:28 +0200

    Add TopLink  to Investments

--- src/components/layouts/Investments/Investments.tsx
+++ src/components/layouts/Investments/Investments.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { Link } from "react-scroll";
 
 import { InvestmentIconsList } from "./InvestmentIconsList/InvestmentIconsList";
@@ -7,6 +7,9 @@ import {
   InvestmentsCurrentI,
   InvestmentsCompletedI
 } from "../../../types/InvestmentsInterfaces";
+import { StyledTopLink } from "./InvestmentList/investmentListStyles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowCircleUp } from "@fortawesome/free-solid-svg-icons";
 
 interface InvestmentsProps {
   investments: InvestmentsCurrentI | InvestmentsCompletedI;
@@ -17,17 +20,40 @@ export const Investments = ({
   investments,
   renderInvestmentsList
 }: InvestmentsProps) => {
+  const iconsList = useRef<HTMLDivElement>(null);
+  const [iconsVisible, setIconsVisible] = useState(true);
+
+  const listener = () => {
+    if (!iconsList.current) return;
+    const rect = iconsList.current.getBoundingClientRect();
+    setIconsVisible(!(rect.bottom < 0));
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", listener);
+
+    return () => {
+      window.removeEventListener("scroll", listener);
+    };
+  }, []);
+
   const cryptoSymbolsList = Object.keys(investments) as Array<
     keyof typeof CRYPTO_SYMBOLS
   >;
 
   return (
-    <div>
-      <InvestmentIconsList cryptoSymbols={cryptoSymbolsList} />
-      <Link to="investments" spy={true} smooth={true}>
-        TOP
-      </Link>
+    <>
+      <div ref={iconsList}>
+        <InvestmentIconsList cryptoSymbols={cryptoSymbolsList} />
+      </div>
+      {!iconsVisible && (
+        <StyledTopLink>
+          <Link to="investments" spy={true} smooth={true}>
+            <FontAwesomeIcon icon={faArrowCircleUp}></FontAwesomeIcon>
+          </Link>
+        </StyledTopLink>
+      )}
       {renderInvestmentsList()}
-    </div>
+    </>
   );
 };
