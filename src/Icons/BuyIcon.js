69e4b0fb6543bc35eb478c36ad6761fec142e73b
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-10-15 15:30:24 +0200

    Add investment icons

---
+++ src/Icons/BuyIcon.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const SvgBuyIcon = props => (
+  <svg viewBox="0 0 32 32" {...props}>
+    <path
+      style={{
+        textIndent: 0,
+        textAlign: "start",
+        lineHeight: "normal",
+        textTransform: "none",
+        blockProgression: "tb",
+        InkscapeFontSpecification: "Sans"
+      }}
+      d="M4 7a1 1 0 000 2h2.219l2.625 10.5c.223.89 1.02 1.5 1.937 1.5H23.25c.902 0 1.668-.599 1.906-1.469L27.75 10h-2.094l-2.406 9H10.781L8.156 8.5A1.983 1.983 0 006.22 7H4zm18 14c-1.645 0-3 1.355-3 3s1.355 3 3 3 3-1.355 3-3-1.355-3-3-3zm-9 0c-1.645 0-3 1.355-3 3s1.355 3 3 3 3-1.355 3-3-1.355-3-3-3zm3-14v5h-3l4 4 4-4h-3V7h-2zm-3 16c.564 0 1 .436 1 1 0 .564-.436 1-1 1-.564 0-1-.436-1-1 0-.564.436-1 1-1zm9 0c.564 0 1 .436 1 1 0 .564-.436 1-1 1-.564 0-1-.436-1-1 0-.564.436-1 1-1z"
+      overflow="visible"
+      fontFamily="Sans"
+    />
+  </svg>
+);
+
+export default SvgBuyIcon;
---
+++ src/Icons/SellIcon.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const SvgSellIcon = props => (
+  <svg viewBox="0 0 32 32" {...props}>
+    <path
+      style={{
+        textIndent: 0,
+        textAlign: "start",
+        lineHeight: "normal",
+        textTransform: "none",
+        blockProgression: "tb",
+        InkscapeFontSpecification: "Sans"
+      }}
+      d="M5 6a1 1 0 000 2h2.219l2.625 10.5c.223.89 1.02 1.5 1.937 1.5H24.25c.902 0 1.668-.599 1.906-1.469L28.75 9h-2.094l-2.406 9H11.781L9.156 7.5A1.983 1.983 0 007.22 6H5zm18 14c-1.645 0-3 1.355-3 3s1.355 3 3 3 3-1.355 3-3-1.355-3-3-3zm-9 0c-1.645 0-3 1.355-3 3s1.355 3 3 3 3-1.355 3-3-1.355-3-3-3zm4-14l-4 4h3v5h2v-5h3l-4-4zm-4 16c.564 0 1 .436 1 1 0 .564-.436 1-1 1-.564 0-1-.436-1-1 0-.564.436-1 1-1zm9 0c.564 0 1 .436 1 1 0 .564-.436 1-1 1-.564 0-1-.436-1-1 0-.564.436-1 1-1z"
+      overflow="visible"
+      fontFamily="Sans"
+    />
+  </svg>
+);
+
+export default SvgSellIcon;
